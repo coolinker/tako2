aa9729b4faf192d14e6c1d312547269b3feefa3e
Jake <jake.yang@synchronoss.com>
2017-06-17 16:03:36 +0800

    update list transfer

---
+++ simplehttp.js
@@ -0,0 +1,93 @@
+var request = require("request");
+var pixelsUtil = require("get-pixels");
+exports.GET = sendGet;
+
+function sendGet(url, options) {
+    if (!options) options = {};
+    return new Promise(function (resolve, reject) {
+        options.uri = url;
+        options.method = "GET";
+        sendRequest(options, (err, res, body) => {
+            resolve({ err, res, body });
+        });
+    })
+}
+
+// function sendGet(url, options, callback) {
+//     options.uri = url;
+//     options.method = "GET";
+//     sendRequest(options, callback);
+// }
+
+exports.POST = sendPost;
+function sendPost(url, options) {
+    // sendRequest(options, callback);
+    return new Promise(function (resolve, reject) {
+        options.uri = url;
+        options.method = "POST";
+        sendRequest(options, (err, res, body) => {
+            resolve({ err, res, body });
+        });
+    })
+}
+
+exports.image = image;
+
+function image(url, options, callback) {
+    options.encoding = null;
+    sendGet(url, options, function (err, response, body) {
+        if (err) {
+            callback(err)
+            return
+        }
+        // console.log("response.headers", response.headers, body)
+        var type = options.type;
+        if (!type) {
+            if (response.getHeader !== undefined) {
+                type = response.getHeader('content-type');
+            } else if (response.headers !== undefined) {
+                type = response.headers['content-type'];
+            }
+        }
+
+        //var type = "image/jpeg"//response.headers['content-type'];
+        if (!type) {
+            callback(new Error('Invalid content-type'))
+            return
+        }
+
+        pixelsUtil(body, type, callback)
+
+    })
+}
+
+// function sendRequest(url, method, options, callback) {
+//     var timeout = options["../timeout"];
+//     delete options["../timeout"];
+//     var jar = options["../cookieJar"];
+//     delete options["../cookieJar"];
+
+//     var form = options;
+//     request({
+//         uri: url,
+//         method: method,
+//         form: form,
+//         jar: jar,
+//         timeout: timeout
+//     }, function(error, response, body) {
+//         callback(error, response, body);
+//     });
+// }
+
+function sendRequest(options, callback) {
+    if (options.cookieJar) {
+        options.jar = options.cookieJar;
+        delete options.cookieJar;
+    }
+
+    var req = request(options, function (error, response, body) {
+        callback(error, response, body);
+    });
+    req.__options = options;
+
+}
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const chrome = require('chrome-remote-interface');
 const pixelsUtil = require("get-pixels");
 const captchautil = require("./captchautil");
-
-const BuyPriceMax = 10000;
+const simplehttp = require("./simplehttp");
+const users = require("./users");
+const BuyPriceMax = 16000, BuyPriceMin = 1500;
 process.setMaxListeners(0);
 
 function timeout(ms) {
@@ -29,17 +30,22 @@ function pageOnload(Page) {
 }
 
 function responseOnReceived(Network, urls) {
+  console.log("responseOnReceived..")
   return new Promise(function (resolve, reject) {
     Network.responseReceived((req) => {
       //https://www.lup2p.com/trading/service/trade/contract-list?
       const rurl = req.response.url;
+      let rid;
       for (let i = 0; i < urls.length; i++) {
         if (rurl.indexOf(urls[i]) === 0) {
-          console.log("responseOnReceived: req.requestId", req.requestId, rurl);
-          resolve(req.requestId);
+          rid = req.requestId
           break;
         }
       }
+      if (rid)
+        console.log("responseOnReceived: req.requestId", req.requestId, rurl);
+
+      if (rid) resolve(rid);
 
     });
 
@@ -48,8 +54,9 @@ function responseOnReceived(Network, urls) {
 
 async function responseDataOnReceived(Network, urls) {
   const resid = await responseOnReceived(Network, urls);
+  console.log("responseDataOnReceived------", resid)
   const data = await Network.getResponseBody({ requestId: resid });
-
+  console.log("responseDataOnReceived------", data)
   return data;
 }
 
@@ -88,7 +95,7 @@ async function querySelector(DOM, selector) {
 
 async function getDomAttributes(DOM, selector) {
   const nid = await querySelector(DOM, selector);
-  if (nid===0) return null;
+  if (nid === 0) return null;
 
   const { attributes: atts } = await DOM.getAttributes({ nodeId: nid });
   const obj = {}
@@ -107,11 +114,11 @@ async function doLogin(DOM, Page, Network, Runtime) {
   const image = await Base64ToImageNArray(data.body, 'image/jpeg');
 
   await Runtime.evaluate({
-    expression: "document.querySelector('#userNameLogin').value='15562566314';"
+    expression: "document.querySelector('#userNameLogin').value='"+users[user].phone+"';"
   });
 
   await Runtime.evaluate({
-    expression: "document.querySelector('#pwd').value='Yangj1anhua'"
+    expression: "document.querySelector('#pwd').value='"+users[user].password+"'"
   });
 
   await Runtime.evaluate({
@@ -149,9 +156,11 @@ async function doLogin(DOM, Page, Network, Runtime) {
   return loginstatus === '00';
 }
 
-async function showDetail(Page, DOM, Runtime, Network, url) {
-  const prdid = url.match(/\?productId=([0-9]+)/)[1];
-  console.log("showDetail prdid", prdid)
+async function showDetail(Page, DOM, Runtime, Network, pid) {
+  const prdid = pid;
+  const url = 'https://list.lu.com/list/productDetail?productId='+pid;
+  console.log("showDetail prdid", pid)
+
   await navigate(Page, url);
   // http://www.lu.com/notFound.html
   const btnAtts = await getDomAttributes(DOM, '.btns.btn_xlarge.investBtn.sk-area-trigger');
@@ -165,9 +174,9 @@ async function showDetail(Page, DOM, Runtime, Network, url) {
   //const in_rep = await Network.getResponseBody({ requestId: in_reqId });
   const invest_check = JSON.parse(in_req.body);
 
-  
+
   console.log("invest_check", invest_check);
-  
+
   if (invest_check.data.code !== "66") return false;
 
   //await pageOnload(Page);
@@ -191,12 +200,14 @@ async function showDetail(Page, DOM, Runtime, Network, url) {
   });
   console.log("btnNodeId===", btnNodeId)
 
+  if (btnNodeId === 0) return false;
+
   await Runtime.evaluate({
     expression: "document.querySelector('a.btns.btn_large').click()"
   });
 
-  const ck_status_res = await responseDataOnReceived(Network, ["https://www.lup2p.com/trading/service/trade/product/"+prdid+"/check-status?",
-    "https://trading.lu.com/trading/service/trade/product/"+prdid+"/check-status?"]);
+  const ck_status_res = await responseDataOnReceived(Network, ["https://www.lup2p.com/trading/service/trade/product/" + prdid + "/check-status?",
+  "https://trading.lu.com/trading/service/trade/product/" + prdid + "/check-status?"]);
   const ck_status = JSON.parse(ck_status_res.body);
 
   if (!ck_status.result) {
@@ -230,16 +241,31 @@ async function showDetail(Page, DOM, Runtime, Network, url) {
     expression: "document.querySelector('a.btns.btn_large.agreeSubmit').click()"
   });
 
+  // 
+  // await responseOnReceived(Network, ["https://www.lup2p.com/trading/security-valid",
+  //   "https://trading.lu.com/trading/security-valid"])
   //https://www.lup2p.com/trading/service/trade/product/149703412/check-status?_=1497194882925
-  ck_status = await responseDataOnReceived(Network, ["https://www.lup2p.com/trading/service/trade/product/" + prdid + "/check-status?"
+  // https://trading.lu.com/trading/service/trade/trace
+
+  const ck_status_rep1 = await responseDataOnReceived(Network, ["https://www.lup2p.com/trading/service/trade/product/" + prdid + "/check-status?"
     , "https://trading.lu.com/trading/service/trade/product/" + prdid + "/check-status?"]);
 
-  console.log("ck_check===========", ck_status);
+  console.log("ck_check===========", ck_status_rep1);
+
+  const ck_status1 = JSON.parse(ck_status_rep1.body);
+
+  if (!ck_status1.result) {
+    console.log("check status", ck_status_rep1);
+    return false;
+  }
+
 
   const create_captcha = await responseDataOnReceived(Network, ["https://www.lup2p.com/trading/service/trade/captcha/create-captcha"
     , "https://trading.lu.com/trading/service/trade/captcha/create-captcha"]);
 
   console.log("create_captcha------", create_captcha)
+  const cr_captcha = JSON.parse(create_captcha.body);
+  if (cr_captcha.retCode === '01') return false;
 
   const captcha = await responseDataOnReceived(Network, ['https://user.lu.com/user/captcha/get-captcha?']);
   const image = await Base64ToImageNArray(captcha.body, 'image/jpeg');
@@ -249,22 +275,62 @@ async function showDetail(Page, DOM, Runtime, Network, url) {
     expression: "document.getElementById('inputValid').value='" + captchaStr + "'"
   });
   console.log("captchaStr", captchaStr)
-  const tradeCode = '19801221jy';
+  const tradeCode = users[user].tradeCode;
   await Runtime.evaluate({
     expression: "document.getElementById('tradeCode').value='" + tradeCode + "'"
   });
 
+  await Runtime.evaluate({
+    expression: "document.getElementById('validBtn').click()"
+  });
 
+
+}
+async function listTransferM3024(Page, DOM, Network) {
+  console.log("listTransferM3024...")
+  const options = {};
+  options.form = {
+    requestCode: "M3024",
+    version: "3.4.9",
+    params: '{"cookieUserName":"","readListType":"trans_p2p","filterBeginInvestPeriodInDay":"10","width":720,"listType":"trans_p2p","pageSize":"15","ver":"1","isForNewUser":"false","productSortType":"INTEREST_RATE_DESC","forNewUser":"false","pageIndex":"1","filterEndTransPrice":"' + 1 + '","source":"android","filterBeginTransPrice":"0.2","currentPage":"1"}'
+  };
+
+  options.headers = {
+    "mobile_agent": "appVersion:3.4.9,platform:android,osVersion:17,device:GT-P5210,resourceVersion:2.7.0,channel:H5",
+    "X-LUFAX-MOBILE-DATA-AGENT": '',
+    "x-lufax-mobile-t": '',
+    "x-lufax-mobile-signature": ''
+  }
+
+  // const rsp = await simplehttp.GET('https://list.lu.com/list/transfer-p2p?minMoney=' + BuyPriceMin + '&maxMoney=' + BuyPriceMax + '&minDays=&maxDays=&minRate=&maxRate=&mode=&tradingMode=&isOverdueTransfer=&isCx=&currentPage=1&orderCondition=&isShared=&canRealized=&productCategoryEnum=&notHasBuyFeeRate=&riskLevel=');
+  // console.log(rsp.body.length)
+  const rsp = await simplehttp.POST('https://ma.lu.com/mapp/service/public?M3024&listType=trans_p2p?_'+Math.round(Math.random() * 100000), options);
+  const bodyJson = JSON.parse(rsp.body);
+//   {"code":"0000","subcode":"","message":"","lid":"T-02f84158-14e7-4c0f-b2b0-4081d6913202_113620221","result":{"totalCount":1,"totalPage":1,"prePage":0,"nextPage":
+// 1,"currentPage":1,"model":"products",
+// "products":[{"productList":
+//     [{"id":150196074,"price":8781.2,"principal":8756.68,"interestRate":"0.084","numOfInstalments":31
+// ,"sourceId":310963071,"publishedAt":"Jun 17, 2017 11:36:16 AM","code":"170617027385","productType":"TRANSFER_REQUEST","productStatus":"ONLINE","collectionMode":
+// "1","productName":"1","tradingMode":"00","mgmtFeeRate":0,"feeDisplayFlag":"false","extOnlineDianjinCount":0,"sourceType":"9","maxInvestAmount":8781.2,"minInvest
+// Amount":8781.2,"remainingAmoun
+
+  if (bodyJson.code === "0000" && bodyJson.result.totalCount > 0) {
+    const prds = bodyJson.result.products[0].productList;
+    for (let i=0; i<prds.length; i++) {
+      if (prds[i].productStatus === 'ONLINE' &&  Number(prds[i].interestRate) >= 0.084 && prds[i].price > BuyPriceMin && prds[i].price < BuyPriceMax) {
+        console.log("---", prds[i]);
+        return prds[i].id;
+      }
+    }
+  }
+
+  return null;
 }
 
 async function listTransfer(Page, DOM, Network) {
-  const s = new Date();
-  console.log("listTransfer------------start")
-  //await navigate(Page, 'https://list.lu.com/list/transfer-p2p?minMoney=&maxMoney='+BuyPriceMax+'&minDays=&maxDays=&minRate=&maxRate=&mode=&tradingMode=&isOverdueTransfer=&isCx=&currentPage=1&orderCondition=&isShared=&canRealized=&productCategoryEnum=&notHasBuyFeeRate=&riskLevel=');
-  await navigateUntilReceive(Page, Network, 'https://list.lu.com/list/transfer-p2p?minMoney=&maxMoney='+BuyPriceMax+'&minDays=&maxDays=&minRate=&maxRate=&mode=&tradingMode=&isOverdueTransfer=&isCx=&currentPage=1&orderCondition=&isShared=&canRealized=&productCategoryEnum=&notHasBuyFeeRate=&riskLevel='
-  , ['https://list.lu.com/list/transfer-p2p?']);
-  
-  console.log("navigateUntilReceive--------------")
+  console.log("listTransfer...")
+  await navigate(Page, 'https://list.lu.com/list/transfer-p2p?minMoney=' + BuyPriceMin + '&maxMoney=' + BuyPriceMax + '&minDays=&maxDays=&minRate=&maxRate=&mode=&tradingMode=&isOverdueTransfer=&isCx=&currentPage=1&orderCondition=&isShared=&canRealized=&productCategoryEnum=&notHasBuyFeeRate=&riskLevel=');
+
   const { root: { nodeId: documentNodeId } } = await DOM.getDocument();
   const { nodeIds: itemNodeIds } = await DOM.querySelectorAll({
     selector: 'li.product-list.has-bottom.transfer-list.clearfix',
@@ -287,9 +353,9 @@ async function listTransfer(Page, DOM, Network) {
     if (prc > BuyPriceMax) continue;
     console.log("html------------", prc, Number(rateMatch[1]))
 
-    const urlMatch = html.match(/<a href="(.+)" target="_blank" class="ld-btn ld-btn-primary">投资<\/a>/);
+    const urlMatch = html.match(/<a href="\/list\/productDetail\?productId=(.+)" target="_blank" class="ld-btn ld-btn-primary">投资<\/a>/);
 
-    return 'https://list.lu.com' + urlMatch[1];
+    return Number(urlMatch[1]);
   }
 
   return null;
@@ -311,15 +377,27 @@ chrome(async protocol => {
 
   await doLogin(DOM, Page, Network, Runtime);
 
-  let turl = null;
+  let pid = null;
+  let lpid = null;
   do {
-    await timeout(100);
-    turl = await listTransfer(Page, DOM, Network);
-    console.log("listTransfer", turl)
-    if (turl) {
-      const status = await showDetail(Page, DOM, Runtime, Network, turl);
-    }
+    const s = new Date();
+    await timeout(300);
+    pid = await listTransferM3024(Page, DOM, Network);
+    await timeout(500);
+    if (!pid) pid = await listTransfer(Page, DOM, Network);
+    await timeout(300);
+    if (!pid) pid = await listTransferM3024(Page, DOM, Network);
+    await timeout(500);
+    if (!pid) pid = await listTransfer(Page, DOM, Network);
     
+    console.log("\nlistTransfer", pid, new Date() - s);
+
+    if (pid && lpid !== pid) {
+      const status = await showDetail(Page, DOM, Runtime, Network, pid);
+      lpid = pid;
+      pid = null;
+    }
+
   } while (true);
 
   console.log("---------------close")
